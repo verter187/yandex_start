d6886cd060b3e461d1417c9c8b450aa6114422e3
verter187 <verter187@inbox.ru>
2023-04-13 19:14:42 +0300

    Panic

--- app_golang.go
+++ app_golang.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func foo(ziro int) int {
+	// Make panic
+	// panic("unexpected")
+	return 7 / ziro
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	// выполняется при завершении main
+	// defer func() {
+	// 	// вызываем recover и сравниваем с nil
+	// 	if r := recover(); r != nil {
+	// 		fmt.Println("My error:", r) // выведет "unexpected"
+	// 	}
+	// }()
+	foo(0) // внтури foo срабатывает паника
+	fmt.Println("Вы не увидите это сообщение, так как в foo возникла паника")
 }
