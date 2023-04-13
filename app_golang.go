8b60db94b95a3b024a6a70c4d151c67b5fc3a4b5
verter187 <verter187@inbox.ru>
2023-04-14 00:00:04 +0300

    Константы

--- app_golang.go
+++ app_golang.go
@@ -1,45 +1,53 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"reflect"
-)
+import "fmt"
+
+const id = 100
 
 func main() {
+	// пример присвоения переменным разных типов значения нетипизированной константы
+	// если константу id типизировать явно, или заменить на переменную, то код ниже будет вызывать ошибку.
+	var i int64 = id
+	var f float64 = id
+	fmt.Println("i=", i, "f=", f)
 
-	var r = 'Щ'
-	fmt.Println(reflect.TypeOf(r))
-	fmt.Println(string(r))
+	var a float64
+	a = 5.0 + 5
+	fmt.Println(a)
 
-	//объявить сразу блок переменных
-	var (
-		height, length int
-		weight         float64
-		name           string
-		company        = "Рога и копыта"
+	const pi = 3.14159
+	const doublePi = pi * 2
+	const version = "1.0.0"
+
+	// // эквивалентно
+	// const (
+	// 	pi       = 3.14159
+	// 	doublePi = pi * 2
+	// 	version  = "1.0.0"
+	// )
+	fmt.Println(version, pi, doublePi)
+
+	// Нетипизированные константы
+	const intConst = 5
+	const floatConst = 5.0
+	const runeConst = 'A'
+	const strConst = "Hello, world!"
+	const boolConst = true
+	fmt.Println(intConst, floatConst, runeConst, strConst, boolConst)
+
+	// Если в группе у константы не указано значение, то оно равно значению предыдущей константы.
+	const (
+		pi_ = 3.1415
+		e
+		name = "John Doe"
+		fullName
 	)
-	fmt.Println(height, length, weight, name, company)
+	fmt.Println("pi =", pi, "e =", e)
+	fmt.Println("name =", name, "fullName =", fullName)
 
-	// Операция приведения типа помогает использовать короткую нотацию
-	int64Var := int64(5)
-	float32Var := float32(101.3)
-
-	// эквивалентно
-
-	var int64Var1 int64 = 5
-	var floatVar float32 = 101.3
-	fmt.Println(int64Var, int64Var1, floatVar, float32Var)
-
-	pi, e := 3.1415, 2.7183
-	// при уточнении значений нельзя использовать :=, так как
-	// обе переменных уже определены
-	pi, e = 3.14159, 2.71828
-
-	f, err := os.Open("myfile.txt")
-	fmt.Println(f, err, pi, e)
-
-	ver, id, pi := "v0.0.1", 0, 3.1415
-
-	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+	// Если при объявлении вы указываете тип константы явным образом, она становится
+	// типизированной и подчиняется правилам сильной типизации Go
+	// const flag uint8 = 128
+	// var i int = flag
+	// fmt.Println(i)
 }
