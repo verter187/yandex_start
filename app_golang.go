8d9c11450c219a19f07ffe1f3125fe22340c1452
verter187 <verter187@inbox.ru>
2023-04-13 21:07:40 +0300

    Переменные и операции с int

--- app_golang.go
+++ app_golang.go
@@ -1,5 +1,26 @@
 package main
 
-func Foo() string {
-	return "bar"
+import "fmt"
+
+func main() {
+
+	var a int
+	var b string
+	//Присвоение в строку переменным разных типов
+	a, b = 5, "test"
+
+	fmt.Println(a, b)
+
+	// Реализация swap и неявная типизация
+	var d, c = 5, 10
+	println(d, c)
+
+	c, d = d, c
+	println(d, c)
+
+	a *= 2
+	fmt.Println("a:", a)
+	a++
+	fmt.Println("a:", a)
+
 }
