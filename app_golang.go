b30bf6edb0d323378ccbfee7ef4b0170dd4025e7
verter187 <verter187@inbox.ru>
2023-04-13 23:47:24 +0300

    Переменные

--- app_golang.go
+++ app_golang.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 )
 
-type Name string
-type Fruit string
-
 func main() {
-	var fruit Fruit
-	var name Name
 
-	fruit = "Apple"
+	var r = 'Щ'
+	fmt.Println(reflect.TypeOf(r))
+	fmt.Println(string(r))
 
-	// name = fruit // Ошибка типизации, хотя оба типа созданы из типа string
-	name = Name(fruit) //а так, после приведения типов, работает.
+	//объявить сразу блок переменных
+	var (
+		height, length int
+		weight         float64
+		name           string
+		company        = "Рога и копыта"
+	)
+	fmt.Println(height, length, weight, name, company)
 
-	fmt.Println(name)
-	fmt.Println(reflect.TypeOf(fruit))
-	fmt.Println(reflect.TypeOf(name))
+	// Операция приведения типа помогает использовать короткую нотацию
+	int64Var := int64(5)
+	float32Var := float32(101.3)
+
+	// эквивалентно
+
+	var int64Var1 int64 = 5
+	var floatVar float32 = 101.3
+	fmt.Println(int64Var, int64Var1, floatVar, float32Var)
+
+	pi, e := 3.1415, 2.7183
+	// при уточнении значений нельзя использовать :=, так как
+	// обе переменных уже определены
+	pi, e = 3.14159, 2.71828
+
+	f, err := os.Open("myfile.txt")
+	fmt.Println(f, err, pi, e)
+
+	ver, id, pi := "v0.0.1", 0, 3.1415
+
+	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
 }
