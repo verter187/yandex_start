d4d3835bd18866ec35b121958269072be0417c5f
verter187 <verter187@inbox.ru>
2023-04-15 18:01:15 +0300

    Считываем введённую пользователем строку

--- app_golang.go
+++ app_golang.go
@@ -1,75 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func f(cnt *int) {
+	*cnt++
+}
 
 func main() {
-	// var a int = 5
-	// p := &a
+	// Получаем читателя пользовательского ввода
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Interaction counter")
 
-	// fmt.Println(a, p) //a=5 p=0xc0000b2008
+	cnt := 0
+	for {
+		fmt.Print("-> ")
+		// Считываем введённую пользователем строку. Программа ждёт, пока пользователь введёт строку
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
 
-	// С константами сложнее — у них забрать адрес не получится.
-	//		  const c = 5
-	//	  p1 := &"abc" // ошибка компиляции
-	//	  p2 := &с // ошибка компиляции
+		f(&cnt)
 
-	// Тип переменной, на которую создаётся указатель, должен соответствовать типу указателя.
-	// var p *int
-	// var a int = 5
-	// var b string = "abc"
-	// p = &a
-	// p = &b // ошибка компиляции
-
-	// // Литералы композитных типов создают в памяти переменную соответствующего типа
-	// type A struct {
-	// 	IntField int
-	// }
-	// // Литерал А{} создаёт в памяти переменную типа А. Затем от неё берётся указатель
-	// z := &A{
-	// 	IntField: 10,
-	// }
-	// fmt.Println(z)
-	// // А ещё в Go есть встроенная функция new()
-	// d := new(A) //  то же самое, что и &A{}
-	// fmt.Println(d)
-
-	// i := 42
-	// r := &i
-	// fmt.Println(*r) // читаем значение переменной i через указатель p
-	// *r = 21         // записываем в переменную i значение 21 через указатель p
-	// fmt.Println(*r)
-
-	// m := []int{1, 2, 3}
-	// b := m
-	// fmt.Println(b)
-	// b[1] = 4
-	// fmt.Println(b)
-	// fmt.Println(m)
-
-	// incrementCopy := func(i int) {
-	// 	i++
-	// }
-
-	// increment := func(i *int) {
-	// 	(*i)++
-	// }
-
-	// i := 42
-
-	// incrementCopy(i)
-	// fmt.Println(i) // 42
-
-	// increment(&i)
-	// fmt.Println(i) // 43
-
-	a := 1
-	p := &a
-	b := &p
-
-	*p = 3
-	**b = 5
-
-	a += 4 + *p + **b
-
-	fmt.Printf("%d", *p)
+		fmt.Printf("User input %d lines\n", cnt)
+	}
 }
