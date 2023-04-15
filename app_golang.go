acb11d53d14cd89ac68bc173a50397c644a2f26f
verter187 <verter187@inbox.ru>
2023-04-15 13:06:41 +0300

    Литералы и их форматированный вывод

--- app_golang.go
+++ app_golang.go
@@ -2,25 +2,19 @@ package main
 
 import "fmt"
 
-type Weekday int
-
-const (
-	Monday Weekday = iota + 1
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
-	Sunday
-)
-
-func NextDay(day Weekday) Weekday {
-	fmt.Println("day: ", day, "day % 7: ", day%7)
-	return (day % 7) + 1
-}
-
 func main() {
-	var today Weekday = Saturday
-	tomorrow := NextDay(today)
-	fmt.Println("today =", today, "tomorrow =", tomorrow)
+
+	a := 1000
+	b := 1000.0
+	c := 1_000          //Можно разделять части числа символом "_" для удобства восприятия
+	d := 01750          // восьмиричное представление, начинаенся с 0
+	e := 0x3e8          // шестнадцатиричное представление
+	f := 0b001111101000 // бинарное представление
+
+	fmt.Printf("%d\n", a)
+	fmt.Printf("%f\n", b)
+	fmt.Printf("%d\n", c)
+	fmt.Printf("0%o\n", d)
+	fmt.Printf("Ox%x\n", e)
+	fmt.Printf("Ob%b\n", f)
 }
