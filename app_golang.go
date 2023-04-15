c6936db7ce44a13f45751e7d194afc7f1b567649
verter187 <verter187@inbox.ru>
2023-04-15 12:34:05 +0300

    Ключевое слово iota

--- app_golang.go
+++ app_golang.go
@@ -2,52 +2,48 @@ package main
 
 import "fmt"
 
-const id = 100
+// Реализация ENUM
+// const (
+//
+//	Black = "black"
+//	Gray  = "gray"
+//	White = "white"
+//
+// )
+const (
+	Black = iota
+	Gray
+	White
+)
+
+// счётчик обнуляется
+const (
+	Yellow = iota
+	Red
+	Green = iota // это присваивание не обнулит iota
+	Blue
+)
 
 func main() {
-	// пример присвоения переменным разных типов значения нетипизированной константы
-	// если константу id типизировать явно, или заменить на переменную, то код ниже будет вызывать ошибку.
-	var i int64 = id
-	var f float64 = id
-	fmt.Println("i=", i, "f=", f)
+	// fmt.Println(Black != Gray) // true
+	fmt.Println(Black, Gray, White)
+	fmt.Println(Yellow, Red, Green, Blue)
 
-	var a float64
-	a = 5.0 + 5
-	fmt.Println(a)
-
-	const pi = 3.14159
-	const doublePi = pi * 2
-	const version = "1.0.0"
-
-	// // эквивалентно
-	// const (
-	// 	pi       = 3.14159
-	// 	doublePi = pi * 2
-	// 	version  = "1.0.0"
-	// )
-	fmt.Println(version, pi, doublePi)
-
-	// Нетипизированные константы
-	const intConst = 5
-	const floatConst = 5.0
-	const runeConst = 'A'
-	const strConst = "Hello, world!"
-	const boolConst = true
-	fmt.Println(intConst, floatConst, runeConst, strConst, boolConst)
-
-	// Если в группе у константы не указано значение, то оно равно значению предыдущей константы.
 	const (
-		pi_ = 3.1415
-		e
-		name = "John Doe"
-		fullName
+		_ = iota * 10 // обратите внимание, что можно пропускать константы
+		ten
+		twenty
+		thirty
 	)
-	fmt.Println("pi =", pi, "e =", e)
-	fmt.Println("name =", name, "fullName =", fullName)
 
-	// Если при объявлении вы указываете тип константы явным образом, она становится
-	// типизированной и подчиняется правилам сильной типизации Go
-	// const flag uint8 = 128
-	// var i int = flag
-	// fmt.Println(i)
+	const (
+		hello = "Hello, world!" // iota равна 0
+		one   = 1               // iota равна 1
+
+		black = iota // iota равна 2
+		gray
+	)
+
+	fmt.Println(ten, twenty, thirty)
+	fmt.Println(hello, one, black, gray)
 }
