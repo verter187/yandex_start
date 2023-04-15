66bc30b7f2ca433f869bd82581f5f421b9960d83
verter187 <verter187@inbox.ru>
2023-04-15 17:05:12 +0300

    FizzBuzz простое решение

--- app_golang.go
+++ app_golang.go
@@ -2,41 +2,18 @@ package main
 
 import "fmt"
 
+// FizzBuzz
 func main() {
-	// Метку можно указать для операторов:
-	// break;
-	// continue;
-	// goto (безусловный оператор перехода, позволяет перейти в любое место кода).
 
-	// outerLoopLabel:
-	// 	for i := 0; i < 5; i++ {
-	// 		for j := 0; j < 5; j++ {
-	// 			fmt.Printf("[%d,%d]\n", i, j)
-	// 			break outerLoopLabel
-	// 		}
-	// 	}
-	// 	fmt.Println("End")
-	// }
-
-outerLoopLabel:
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			fmt.Printf("[%d,%d]\n", i, j)
-			continue outerLoopLabel
-		}
-	}
-	fmt.Println("End")
-
-	group := 0
-	for i := 0; i < 20; i++ {
-		switch {
-		case i%2 == 0:
-			if i%10 == 0 {
-				group++
-				break // break относится к ближайшему switch
-			}
-			fmt.Printf("%02d: %d\n", group, i)
-		default:
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, "- FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, "- Fizz")
+		} else if i%5 == 0 {
+			fmt.Println(i, "- Buzz")
+		} else {
+			fmt.Println(i)
 		}
 	}
 }
