159f8d44aa98e9341e85ef55fcdbec6b6eac3e26
verter187 <verter187@inbox.ru>
2023-04-16 19:04:33 +0300

    Операции со слайсами

--- app_golang.go
+++ app_golang.go
@@ -1,100 +1,63 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
+// Моя реализация copy
+func myCopy(receiver []int, origin []int) {
+	for i := range receiver {
+		fmt.Println(i)
+		if i >= len(origin) {
+			receiver[i] = 0
+		} else {
+			receiver[i] = origin[i]
+		}
+	}
+}
+
+func pyrange(start, end, step int) []int {
+	// TODO: Error checking to make sure parameters are all valid,
+	// else you could get divide by zero in make and other errors.
+
+	rtn := make([]int, 0, (end-start)/step)
+	for i := start; i < end; i += step {
+		rtn = append(rtn, i)
+	}
+	return rtn
+}
 func main() {
+	var dest []int
+	dest2, dest3 := make([]int, 3), make([]int, 5)
+	src := []int{1, 2, 3, 4}
 
-	// mySlice := make([]TypeOfelement, LenOfslice, CapOfSlice)
-	// mySlice := make([]int, 0)     // слайс [], базовый массив []
-	// mySlice := make([]int, 5)     // слайс [0 0 0 0 0], базовый массив [0 0 0 0 0]
-	// mySlice := make([]int, 5, 10) // слайс [0 0 0 0 0], базовый массив [0 0 0 0 0 0 0 0 0 0]
+	myCopy(dest, src)
+	myCopy(dest2, src)
+	myCopy(dest3, src)
+	fmt.Println("2", dest, dest2, dest3, src) // [] [1 2 3] [1 2 3 4 0] [1 2 3 4]
 
-	// 	Аргументы функции make:
-	// Тип слайса (пустые квадратные скобки и тип элемента слайса).
-	// Длина слайса. Если не передана, то по умолчанию равна нулю.
-	// Ёмкость слайса — размер базового массива. Если значение не передано, то по умолчанию равна длине слайса.
-	mySlice := make([]int, 5, 5) // слайс [0 0 0 0 0], базовый массив [0 0 0 0 0]
-	fmt.Println(mySlice)
-	fmt.Println(len(mySlice))
-	fmt.Println(cap(mySlice))
+	// Удаление последнего элемента слайса:
+	s := []int{1, 2, 3}
+	if len(s) != 0 { // защищаемся от паники
+		s = s[:len(s)-1]
+	}
+	fmt.Println(s)
 
-	mySlice = append(mySlice, 5)
-	fmt.Println(mySlice)
-	fmt.Println(len(mySlice))
-	fmt.Println(cap(mySlice))
-	mySlice = append(mySlice, 6, 7, 8, 9, 10)
-	fmt.Println(mySlice)
-	fmt.Println(mySlice[0:20])
-	fmt.Println(len(mySlice))
-	fmt.Println(cap(mySlice))
+	// Удаление первого элемента слайса:
+	s1 := []int{1, 2, 3}
+	if len(s1) != 0 { // защищаемся от паники
+		s1 = s1[1:]
+	}
+	fmt.Println(s1)
 
-	var arr [3]int
-	fmt.Println(reflect.TypeOf(arr))
+	// Удаление элемента слайса с индексом i:
+	s3 := []int{1, 2, 3, 4, 5}
+	i := 2
+	if len(s3) != 0 && i < len(s3) { // защищаемся от паники
+		s3 = append(s3[:i], s3[i+1:]...)
+	}
+	fmt.Println(s3)
 
-	var sls []int
-	fmt.Println(reflect.TypeOf(sls))
+	ms1 := pyrange(1, 101, 1)
+	ms1 = append(ms1[:10], ms1[90:]...)
+	fmt.Println(ms1)
 
-	weekTempArr := [7]int{1, 2, 3, 4, 5, 6, 7}
-	workDaysSlice := weekTempArr[:5]
-	weekendSlice := weekTempArr[5:]
-	fromTuesdayToThursDaySlice := weekTempArr[1:4]
-	weekTempSlice := weekTempArr[:]
-
-	fmt.Println(workDaysSlice[2])
-	workDaysSlice[2] = 7
-	fmt.Println(workDaysSlice, len(workDaysSlice), cap(workDaysSlice))                                        // [1 2 3 4 5] 5 7
-	fmt.Println(weekendSlice, len(weekendSlice), cap(weekendSlice))                                           // [6 7] 2 2
-	fmt.Println(fromTuesdayToThursDaySlice, len(fromTuesdayToThursDaySlice), cap(fromTuesdayToThursDaySlice)) // [2 3 4] 3 6
-	fmt.Println(weekTempSlice, len(weekTempSlice), cap(weekTempSlice))
-
-	// Внимание: append не изменяет переданный ей слайс, а создаёт новый на основе переданного.
-	// Внимание: Емкость среза - это количество элементов в базовом массиве, !считая от первого элемента в срезе! - это
-	//и объясняет, почему для a в примере cap - 4, а для b -2, хотя у них один базовый массив.
-	a := []int{1, 2, 3, 4}
-	b := a[2:3] // b = [3]
-	fmt.Println(b, len(b), cap(b))
-	b[0] = 5
-	b = append(b, 7)
-	fmt.Println(a, len(a), cap(a)) // [1 2 3 7] 4 4
-	fmt.Println(b, len(b), cap(b)) // [3 7] 2 2
-	b = append(b, 8, 9, 10)
-	b[0] = 11
-	fmt.Println(a, len(a), cap(a)) // [1 2 3 7] 4 4
-	fmt.Println(b, len(b), cap(b)) // [11 7 8 9 10] 5 6 // [1 2 3 4 5 6 7] 7 7
-
-	s := make([]int, 4, 7) // [0 0 0 0], len = 4 cap = 7
-	// 1. Создаём слайс s с базовым массивом на 7 элементов.
-	// Четыре первых элемента будут доступны в слайсе.
-
-	slice1 := append(s[:2], 2, 3, 4)
-	fmt.Println(s, slice1)
-	// 2. Берём слайс из первых двух элементов s и добавляем к ним три элемента.
-	// Так как суммарная длина полученного слайса (len == 5) меньше ёмкости s[:2] (cap == 7),
-	// то базовый массив остаётся прежним.
-	// Слайс s тоже изменился, но его длина осталась прежней
-
-	slice2 := append(s[1:2], 7)
-	fmt.Println(s, slice1, slice2) // [0 0 7 3] [0 0 7 3 4] [0 7]
-	// 3. Здесь также базовый массив остаётся прежним, изменились все три слайса
-
-	slice3 := append(s, slice1[1:]...)
-	fmt.Println(len(slice3), cap(slice3)) // 8 14
-	// 4. Длина s и slice1[1:] равна 4, длина нового слайса будет равна 8,
-	// что больше ёмкости базового массива.
-	// Будет создан новый базовый массив ёмкостью 14,
-	// ёмкость нового базового массива подбирается автоматически
-	// и зависит от текущего размера и количества добавленных элементов
-
-	// 5. Легко проверить, что slice3 ссылается на новый базовый массив
-	s[1] = 99
-	fmt.Println(s, slice1, slice2, slice3)
-	// [0 99 7 3] [0 99 7 3 4] [99 7] [0 0 7 3 0 7 3 4]
-
-	// В примере выше, не очень понятно, будут ли новые слайсы ссылаться на
-	//тот же базовый массив или отправят свои копии в новый массив.
-	//Поэтому на практике функцию append рекомендуют лишь для присвоения
-	//слайса самому себе: s = append(s, b).
 }
