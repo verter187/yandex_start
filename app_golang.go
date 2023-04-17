390d657da9eeccf448a69aa9c4705d83bef51057
verter187 <verter187@inbox.ru>
2023-04-17 20:47:40 +0300

    Функции. Задача вывод файлов с учетом фильтра

--- app_golang.go
+++ app_golang.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-func PrintAllFiles(path string) {
+func PrintAllFilesWithFilter(path string, filter string) {
 	// получаем список всех элементов в папке (и файлов, и директорий)
 	files, err := os.ReadDir(path)
 	if err != nil {
@@ -15,56 +16,22 @@ func PrintAllFiles(path string) {
 	}
 	//  проходим по списку
 	for _, f := range files {
+
 		// получаем имя элемента
 		// filepath.Join — функция, которая собирает путь к элементу с разделителями
 		filename := filepath.Join(path, f.Name())
-		// печатаем имя элемента
-		fmt.Println(filename)
+
+		if strings.Contains(filename, filter) {
+			// печатаем имя элемента
+			fmt.Println(filename)
+		}
 		// если элемент — директория, то вызываем для него рекурсивно ту же функцию
 		if f.IsDir() {
-			PrintAllFiles(filename)
+			PrintAllFilesWithFilter(filename, filter)
 		}
 	}
 }
-func Sum(x ...int) (res int) {
-	for _, v := range x {
-		res += v
-	}
-	return
-}
 
-func Index(st string, a rune) (index int, ok bool) {
-	for i, c := range st {
-		if c == a {
-			return i, true
-		}
-	}
-	return
-}
-
-func fact(n int) int {
-	if n == 0 { // терминальная ветка — то есть условие выхода из рекурсии
-		return 1
-	} else { // рекурсивная ветка
-		return n * fact(n-1)
-	}
-}
-
-func Fib(n int) int {
-	switch {
-	case n <= 1: // терминальная ветка
-		return n
-	default: // рекурсивная ветка
-		return Fib(n-1) + Fib(n-2)
-	}
-}
 func main() {
-	sum := Sum(2, 3, 5, 1, 2, 57)
-	fmt.Println(sum)
-
-	i, ok := Index("testing", 'e')
-	println(i, ok)
-
-	fmt.Println(fact(5))
-	PrintAllFiles(".")
+	PrintAllFilesWithFilter(".", "git/refs/heads")
 }
