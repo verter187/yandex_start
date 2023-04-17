3188ad7bc1d7f082c9ed4abb22ebc636975a5016
verter187 <verter187@inbox.ru>
2023-04-17 21:46:25 +0300

    Замыкания. Пример обход файлов с учетом фильтра

--- app_golang.go
+++ app_golang.go
@@ -2,45 +2,41 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"runtime"
-	"time"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-// countCall — функция-обёртка для подсчёта вызовов
-func countCall(f func(string)) func(string) {
-	cnt := 0
-	// получаем имя функции. Подробнее об этом вызове будет рассказано в следующем курсе
-	funcname := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
-	return func(s string) {
-		cnt++
-		fmt.Printf("Функция %s вызвана %d раз\n", funcname, cnt)
-		f(s)
+func PrintAllFilesWithFilterClosure(path string, filter string) {
+	// создаём переменную, содержащую функцию обхода
+	// мы создаём её заранее, а не через оператор :=, чтобы замыкание могло сослаться на него
+	var walk func(string)
+	walk = func(path string) {
+		// получаем список всех элементов в папке (и файлов, и директорий)
+		files, err := os.ReadDir(path)
+		if err != nil {
+			fmt.Println("unable to get list of files", err)
+			return
+		}
+		//  проходим по списку
+		for _, f := range files {
+			// получаем имя элемента
+			// filepath.Join — функция, которая собирает путь к элементу с разделителями
+			filename := filepath.Join(path, f.Name())
+			// печатаем имя элемента, если путь к нему содержит filter, который получим из внешнего контекста
+			if strings.Contains(filename, filter) {
+				fmt.Println(filename)
+			}
+			// если элемент — директория, то вызываем для него рекурсивно ту же функцию
+			if f.IsDir() {
+				walk(filename)
+			}
+		}
 	}
-}
-
-// metricTimeCall — функция-обёртка для замера времени
-func metricTimeCall(f func(string)) func(string) {
-	return func(s string) {
-		start := time.Now() // получаем текущее время
-		f(s)
-		fmt.Println("Execution time: ", time.Now().Sub(start)) // получаем интервал времени как разницу между двумя временными метками
-	}
-}
-
-func myprint(s string) {
-	fmt.Println(s)
+	// теперь вызовем функцию walk
+	walk(path)
 }
 
 func main() {
-
-	countedPrint := countCall(myprint)
-	countedPrint("Hello world")
-	countedPrint("Hi")
-
-	// обратите внимание, что мы оборачиваем уже обёрнутую функцию, а значение счётчика вызовов при этом сохраняется
-	countAndMetricPrint := metricTimeCall(countedPrint)
-	countAndMetricPrint("Hello")
-	countAndMetricPrint("World")
-
+	PrintAllFilesWithFilterClosure(".", "index")
 }
