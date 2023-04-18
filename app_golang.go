f07367cc25bdc28f037149cafc0ad523b6a1010d
verter187 <verter187@inbox.ru>
2023-04-18 20:50:16 +0300

    Оператор отложенного вызова

--- app_golang.go
+++ app_golang.go
@@ -2,41 +2,90 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"path/filepath"
-	"strings"
+	"time"
 )
 
-func PrintAllFilesWithFilterClosure(path string, filter string) {
-	// создаём переменную, содержащую функцию обхода
-	// мы создаём её заранее, а не через оператор :=, чтобы замыкание могло сослаться на него
-	var walk func(string)
-	walk = func(path string) {
-		// получаем список всех элементов в папке (и файлов, и директорий)
-		files, err := os.ReadDir(path)
-		if err != nil {
-			fmt.Println("unable to get list of files", err)
-			return
+func EvaluationOrder() {
+	defer fmt.Println("deferred")
+	fmt.Println("evaluated")
+}
+
+func unintuitive() (value string) {
+	defer func() { value = "На самом деле" }() // круглые скобки в конце означают, что функция вызывается
+	return "Казалось бы"
+}
+
+func intuitive() string {
+	value := "Казалось бы"
+	defer func() { value = "На самом деле" }()
+	return value
+}
+
+func metricTime(start time.Time) {
+	// функция Now() возвращает текущее время, а функция Sub возвращает разницу между двумя временными метками
+	fmt.Println(time.Now().Sub(start))
+}
+
+func VeryLongTimeFunction() {
+	defer metricTime(time.Now()) // передаём в функцию metricTime значение текущего времени и откладываем её вызов до возврата
+	// Какие-то долгие вычисления
+	time.Sleep(3 * time.Second)
+}
+
+func PanicingFunc() {
+	defer func() {
+		if r := recover(); r != nil { // встроенная функция recover останавливает панику и возвращает описание произошедшего
+			fmt.Println("Panic is caught", r)
 		}
-		//  проходим по списку
-		for _, f := range files {
-			// получаем имя элемента
-			// filepath.Join — функция, которая собирает путь к элементу с разделителями
-			filename := filepath.Join(path, f.Name())
-			// печатаем имя элемента, если путь к нему содержит filter, который получим из внешнего контекста
-			if strings.Contains(filename, filter) {
-				fmt.Println(filename)
-			}
-			// если элемент — директория, то вызываем для него рекурсивно ту же функцию
-			if f.IsDir() {
-				walk(filename)
-			}
-		}
-	}
-	// теперь вызовем функцию walk
-	walk(path)
+	}()
+	///
+	///
+
+	panic("Мне здесь совсем ничего не нравится!")
+	// встроенная функция panic () вызывает панику у функции.
+	// в качестве аргумента ей принято передавать причину паники. Именно она будет возвращена функцией recover
+
 }
 
 func main() {
-	PrintAllFilesWithFilterClosure(".", "index")
+	// EvaluationOrder()
+	fmt.Println("Hello")
+	for i := 1; i <= 3; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("World")
+	test := unintuitive()
+	fmt.Println(test)
+
+	test1 := intuitive()
+	fmt.Println(test1)
+
+	// Операнды отложенной функции вычисляются при выполнении оператора defer, а не
+	// при выполнении отложенной функции
+	a := "some text"
+	defer func(s string) {
+		fmt.Println(s)
+	}(a)
+	a = "another text"
+
+	// Оператор defer чаще всего можно увидеть с парными функциями
+	// Open()/Close(), Lock()/Unlock(). Его ставят сразу после захвата ресурса, чтобы точно не забыть.
+	// открываем файл
+	file, err := os.OpenFile("file.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// не забываем закрыть файл
+	defer file.Close()
+	// работаем с файлом
+	_, err = file.WriteString("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	VeryLongTimeFunction()
+
+	PanicingFunc()
 }
