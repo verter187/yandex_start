a35bde4bc646a9635be0ebeed1b2f896b15af13f
verter187 <verter187@inbox.ru>
2023-04-24 20:07:05 +0300

    Методы

--- app_golang.go
+++ app_golang.go
@@ -2,17 +2,50 @@ package main
 
 import "fmt"
 
-var Global = 5
+type MyType int
 
-func editGlobal() {
+func (m MyType) String() string {
+	return fmt.Sprintf("MyType: %d", m)
+}
 
-	defer func(checkout int) { Global = checkout }(Global)
-	Global = 42
-	fmt.Println("Cur global value:", Global)
+type DeliveryState string
+
+const (
+	DeliveryStatePending   DeliveryState = "pending"      // сообщение отправлено
+	DeliveryStateAck       DeliveryState = "acknowledged" // сообщение получено
+	DeliveryStateProcessed DeliveryState = "processed"    // сообщение обработано успешно
+	DeliveryStateCanceled  DeliveryState = "canceled"     // обработка сообщения прервана
+
+)
+
+func (s DeliveryState) IsValid() bool {
+	switch s {
+	case DeliveryStatePending, DeliveryStateAck, DeliveryStateProcessed, DeliveryStateCanceled:
+		return true
+	default:
+		return false
+	}
+}
+
+func (s DeliveryState) String() string {
+	return string(s)
+}
+
+func HandleMsgDeliveryStatus(status DeliveryState) error {
+	if !status.IsValid() {
+		return fmt.Errorf("status: invalid")
+	}
+	return nil
 }
 
 func main() {
-	fmt.Println("Global value:", Global)
-	editGlobal()
-	fmt.Println("Global value:", Global)
+	var m MyType = 5
+
+	s := m.String()
+	fmt.Println(s)
+
+	// приводим строку "fake" к типу DeliveryState
+	if err := HandleMsgDeliveryStatus(DeliveryState("fake")); err != nil {
+		panic(err)
+	}
 }
